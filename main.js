c50542e4f44df52ff17d5aa001754e6abb8445d7
minacacatua <carmina.flecha@escola.pr.gov.br>
2026-06-17 14:09:46 +0000

    tomar banho nesse frio? puta que pariu

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2027-02-26T00:00:00");
 const tempoObjetivo2 = new Date("2027-02-27T00:00:00");
 const tempoObjetivo3 = new Date("2027-01-15T00:00:00");
-const tempoObjetivo4 = new Date("2026-12-18T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-19T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
